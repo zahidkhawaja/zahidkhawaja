1112acb4697f012788562ff314277959a670f72f
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-15 15:53:10 -0400

    Fix dark mode switch styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDarkMode } from "../hooks/useDarkMode";
 import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
 
 const Nav = () => {
     const [darkMode, setDarkMode] = useDarkMode();
@@ -10,9 +11,25 @@ const Nav = () => {
         setDarkMode(!darkMode);
     };
 
+    const DarkSwitch = withStyles({
+        switchBase: {
+          color: "#f8f8ff",
+          '&$checked': {
+            color: "#32CD32",
+          },
+          '&$checked + $track': {
+            backgroundColor: "#32CD32",
+          },
+        },
+        checked: {},
+        track: {},
+      })(Switch);
+
     return (
         <div className = "nav">
-            <Switch checked = {darkMode} color = "secondary" onClick = {toggleDarkMode} ></Switch>
+            <div className = "switch">
+            <DarkSwitch checked = {darkMode} color = "secondary" onClick = {toggleDarkMode} ></DarkSwitch>
+            </div>
         </div>
     )
 }
