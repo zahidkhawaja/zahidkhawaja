e064d31c3a60f0ed7ce3b8407e9d6ad729c0f5eb
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-31 18:03:49 -0400

    Create ParticlesComponent and render on 'random' route

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import ParticlesComponent from "./ParticlesComponent";
 
 const Random = () => {
+
+    const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * (800 - 300) + 300));
+
     return (
         <div className = "viewcontent">
-        <p>Stay tuned for awesome randomness!</p>
+            <ParticlesComponent randomNumber = {randomNumber} />
+            <p>Particle count: {randomNumber}</p>
+            <button className = "generatebutton" onClick = {() => setRandomNumber(Math.floor(Math.random() * (800 - 300) + 300))}>Generate</button>
         </div>
     )
 }
