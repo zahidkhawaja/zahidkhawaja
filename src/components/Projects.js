397afef378a7cb83012315419b2f83b9d3eebf34
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-16 18:44:08 -0400

    Update projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,9 @@ const Projects = () => {
     return (
         <div className = "viewcontent">
             <div className = "projectslist">
-        <a href = "https://github.com/builld-vr-funding-platform/marketing-page">SIXR: Crowdfunding Platform</a>
-        <a href = "https://github.com/zahidkhawaja/Retirement-Calculator">Retirement Calculator: Build Financial Success</a>
-        <a href = "https://github.com/BuildWeekWunderlist6/frontend">Task Management on Autopilot</a>
+        <a href = "https://github.com/zahidkhawaja/Chat-App">Chat App (Frontend/Backend)</a>
+        <a href = "https://github.com/zahidkhawaja/Retirement-Calculator-2.0">Retirement Calculator (Frontend)</a>
+        <a href = "https://github.com/zahidkhawaja/zahidkhawaja">Personal Website (Frontend)</a>
         </div>
         </div>
     )
