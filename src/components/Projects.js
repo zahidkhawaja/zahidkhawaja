10230851b0786eff83936e8f5fe759ca177ad903
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-19 19:28:43 -0400

    Update project names

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,9 @@ const Projects = () => {
     return (
         <div className = "viewcontent">
             <div className = "projectslist">
-        <a href = "https://github.com/zahidkhawaja/Chat-App">Chat App (Frontend/Backend)</a>
-        <a href = "https://github.com/zahidkhawaja/Retirement-Calculator-2.0">Retirement Calculator (Frontend)</a>
-        <a href = "https://github.com/zahidkhawaja/zahidkhawaja">Personal Website (Frontend)</a>
+        <a href = "https://github.com/zahidkhawaja/Amplify-Frontend">Amplify (Frontend/Backend)</a>
+        <a href = "https://github.com/zahidkhawaja/Treasure-2.0">Treasure 2.0 (Frontend)</a>
+        <a href = "https://github.com/zahidkhawaja/zahidkhawaja.com">Personal Website (Frontend)</a>
         </div>
         </div>
     )
